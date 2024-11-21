6855ecaed7a2e2d935e9d49dd75f3389bc078c9b
Sov3rain <10755450+Sov3rain@users.noreply.github.com>
2024-11-21 16:38:04 +0100

    add LogService

---
+++ Assets/Scripts/PocketBase/Dtos/LogModel.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+/// <summary>
+/// Response DTO of a single log model.
+/// </summary>
+[Serializable]
+public sealed class LogModel
+{
+    [JsonProperty("id")]
+    public string Id { get; set; }
+
+    [JsonProperty("created")]
+    public string Created { get; set; }
+
+    [JsonProperty("updated")]
+    public string Updated { get; set; }
+
+    [JsonProperty("level")]
+    public int Level { get; set; }
+
+    [JsonProperty("message")]
+    public string Message { get; set; }
+    
+    [JsonProperty("data")]
+    Dictionary<string, object> Data { get; set; } = new();
+}
